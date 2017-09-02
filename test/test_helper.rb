e84f84fa91b6ab99e07a5a19d10457e022e44c6f
Eric Butler <ericbutler555@gmail.com>
2017-09-01 23:37:03 -0400

    Implement basic login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,4 +7,10 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
   include ApplicationHelper
+  
+  # check if the test user is logged in
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+
 end
