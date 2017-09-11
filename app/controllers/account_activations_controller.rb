030bf288e86a7a0af8421b4f6d336b29f0641b3f
Eric Butler <ericbutler555@gmail.com>
2017-09-10 23:54:52 -0400

    Add account activation

---
+++ app/controllers/account_activations_controller.rb
@@ -0,0 +1,16 @@
+class AccountActivationsController < ApplicationController
+
+  def edit
+    user = User.find_by(email: params[:email])
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      user.activate # find def in User model
+      log_in user # find def in sessions_helper
+      flash[:success] = "Account activated!"
+      redirect_to user
+    else
+      flash[:danger] = "Invalid activation link"
+      redirect_to root_url
+    end
+  end
+
+end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,14 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
-      log_in @user
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      redirect_back_or @user
+      if @user.activated?
+        log_in @user
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        redirect_back_or @user
+      else
+        flash[:warning] = "Account has not yet been activated. Check your email for the activation link."
+        redirect_to root_path
+      end
     else
       flash.now[:danger] = "Invalid username and/or password"
       render 'new'
