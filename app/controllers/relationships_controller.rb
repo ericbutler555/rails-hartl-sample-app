7eea6e9d9bc4cb892785510f020aad80d8ceeb87
Eric Butler <ericbutler555@gmail.com>
2017-09-17 23:57:13 -0400

    Use AJAX for follow forms

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -3,17 +3,23 @@ class RelationshipsController < ApplicationController
   before_action :logged_in_user
 
   def create
-    user = User.find(params[:followed_id])
-    current_user.follow(user)
-    flash[:success] = "You are now following this user"
-    redirect_to user
+    @user = User.find(params[:followed_id])
+    current_user.follow(@user)
+    # This doesn't work with Ajax: flash.now[:success] = "You are now following this user"
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 
   def destroy
-    user = Relationship.find(params[:id]).followed
-    current_user.unfollow(user)
-    flash[:warning] = "You are no longer following this user"
-    redirect_to user
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow(@user)
+    # This doesn't work with Ajax: flash.now[:warning] = "You are no longer following this user"
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
   end
 
 end
